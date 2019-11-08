468359eae9a66ba369536a6d6d6c43c82e4551b5
mariusonu <onu.marius777@gmail.com>
2019-11-08 13:52:26 +0200

    FizzBuzz

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -2,7 +2,25 @@ package code._4_student_effort;
 
 public class Main {
 
-  public static void main(String[] args) {
-    //TODO put your code changes in here
-  }
+    public static void main(String[] args) {
+        //TODO put your code changes in here
+        //FizzBuzz
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.print("Fizzbuzz ");
+            }else if(i % 9 == 0){
+              System.out.print("Jazz ");
+            }else if(i % 7 == 0){
+              System.out.print("Rizz");
+            }else if(i % 5 == 0){
+              System.out.print("Buzz");
+            } else if (i % 3 == 0){
+              System.out.print("Fizz");
+            }else{
+              System.out.print(i + " ");
+            }
+        }
+
+    }
 }
--- _2_oo/src/main/java/code/_4_student_effort/Main.java
+++ _2_oo/src/main/java/code/_4_student_effort/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
     //TODO put your code changes in here
+
+
+
   }
 }
