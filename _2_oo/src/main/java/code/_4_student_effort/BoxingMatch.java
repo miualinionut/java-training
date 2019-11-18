0af83550d609fc5ca199e562024dd7cec0e12737
unknown <anamaria.pam11@yahoo.ro>
2019-11-18 19:55:55 +0200

    clasa hello world

---
+++ _2_oo/src/main/java/code/_4_student_effort/BoxingMatch.java
@@ -0,0 +1,9 @@
+package code._4_student_effort;
+
+public class BoxingMatch {
+    private Fighter man1;
+    private Fighter man2;
+
+    public BoxingMatch(String)
+
+}
---
+++ _2_oo/src/main/java/code/_4_student_effort/Fighter.java
@@ -0,0 +1,18 @@
+package code._4_student_effort;
+
+public class Fighter {
+        public final String name;
+        public int health;
+        public final int damageperattack;
+
+        public Fighter(String name, int health, int damageperattack){
+            this.name=name;
+            this.health=health;
+            this.damageperattack=damageperattack;
+        }
+         public void attack(Fighter opponent){
+
+         }
+}
+
+
