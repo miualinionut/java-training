cafa1f225a795e2c8c734583333a7d1ba19c95fc
Bogdan <zatbogdan99@gmail.com>
2019-11-18 20:14:08 +0200

    clasa hello world 2

--- _3_design_patterns/src/main/java/code/_3_in_class/HelloWorld.java
+++ _3_design_patterns/src/main/java/code/_3_in_class/HelloWorld.java
@@ -2,6 +2,6 @@ package code._3_in_class;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Buna lume");
+        System.out.println("Buna lume 2");
     }
 }
