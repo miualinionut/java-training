6fc6585e76409a417dcc67fbc3fb997ea91b49ce
DragosBalmau <balmaudragos@gmail.com>
2019-11-24 20:02:27 +0200

    update curs 3

--- _3_design_patterns/src/main/java/code/_3_in_class/HelloWorld.java
+++ _3_design_patterns/src/main/java/code/_3_in_class/HelloWorld.java
@@ -5,6 +5,5 @@ public class HelloWorld {
 
         System.out.println("hello world");
 
-
     }
 }
---
+++ _3_design_patterns/src/main/java/code/_3_in_class/Singleton.java
@@ -0,0 +1,19 @@
+package code._3_in_class;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Singleton {
+    static int  nrInstante = 0;
+    static Singleton INSTANCE = null;
+    private Singleton(){
+        this.nrInstante = this.nrInstante + 1;
+        System.out.println("s-a apelat constr " + this.nrInstante + " ori");
+    }
+    public static Singleton getInstante(){
+
+        if(nrInstante == 0)
+            return INSTANCE = new Singleton();
+        else
+            return INSTANCE;
+    }
+}
