5206da65232192ea89046d5b50cdaa45a20f2a06
mariusonu <onu.marius777@gmail.com>
2019-11-27 15:55:54 +0200

    challenge 2

--- _4_exceptions_io/src/main/java/code/_4_student_effort/Main.java
+++ _4_exceptions_io/src/main/java/code/_4_student_effort/Main.java
@@ -1,8 +1,34 @@
 package code._4_student_effort;
 
+import java.io.*;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //TODO put your code changes in here
+  public static void main(String[] args) throws FileNotFoundException {
+    //exercitiul 2
+
+    File file = new File("C:\\development\\workspace\\java-training\\_4_exceptions_io\\_test_files\\in\\message.txt");
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    String line;
+    char c = 'X';
+    StringBuilder sb = new StringBuilder();
+
+    try{
+      while((line = br.readLine()) != null){
+        for (int i = 0; i < line.length(); i++) {
+          if (Character.isUpperCase(line.charAt(i)) && line.charAt(i) != c) {
+            sb.append(line.charAt(i));
+          } else if (line.charAt(i) == c) {
+            sb.append(" ");
+          }
+        }
+      }
+      br.close();
+    }catch(IOException x){
+      x.printStackTrace();
+    }finally {
+
+      System.out.println(sb);
+    }
   }
 }
