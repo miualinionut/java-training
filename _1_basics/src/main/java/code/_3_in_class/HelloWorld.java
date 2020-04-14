37fcfc8b5605f49cdd3e2af0b6e786acf8a769f7
andreeaduiu <andreeaduiu@gmail.com>
2020-04-14 18:55:16 +0300

    ANDREEA DUIU

--- _1_basics/src/main/java/code/_3_in_class/HelloWorld.java
+++ _1_basics/src/main/java/code/_3_in_class/HelloWorld.java
@@ -1,7 +1,7 @@
 package code._3_in_class;
 
 public class HelloWorld {
-    public static void main(String[] args)
-    {
-        System.out.println("hello world");}
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
