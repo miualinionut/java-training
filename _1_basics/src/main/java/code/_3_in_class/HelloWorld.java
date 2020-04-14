ad7c2a5b2d252359c317aa0b11b68580a5ef53ef
Maria Lungeanu <maria21lungeanu@gmail.com>
2020-04-14 11:22:00 +0300

    helloWorld class add + rollBack changes Main

--- _1_basics/src/main/java/code/_3_in_class/HelloWorld.java
+++ _1_basics/src/main/java/code/_3_in_class/HelloWorld.java
@@ -1,8 +1,7 @@
 package code._3_in_class;
 
-public class Main {
-
+public class HelloWorld {
     public static void main(String[] args) {
-        //TODO put your code changes in here
+        System.out.println("hello world");
     }
 }
