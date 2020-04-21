37fe41e8aea0b3b78929663e27d4035bdbf35de8
AnastasiaGheorghe <anastasiagabriela79@gmail.com>
2020-04-21 20:47:24 +0300

    Exercitiul 3 refacut

--- _1_basics/src/main/java/code/_4_student_effort/Pairs.java
+++ _1_basics/src/main/java/code/_4_student_effort/Pairs.java
@@ -1,20 +1,29 @@
-public class Pairs {
-    static int CountPairs(int arr[], int n)
+class Pairs
+{
+    static int countpairs(int arr[], int n)
     {
         int count = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if ((i + j) == (arr[i] + arr[j]))
-                    count++;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 1;i++)
+        {
+            if (arr[i] == arr[i + 1])
+            {
+                count++;
+                i = i + 2;
+            }
+            else
+            {
+                i++;
             }
         }
         return count;
     }
-
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        int arr[] = { 1, 0, 3, 2 };
-        int n = arr.length ;
-        System.out.print(CountPairs(arr, n));
+        int arr[] = { 5, 4, 2, 3, 5, 2, 2, 3 };
+        int n = arr.length;
+
+        System.out.println(countpairs(arr, n));
     }
 }
