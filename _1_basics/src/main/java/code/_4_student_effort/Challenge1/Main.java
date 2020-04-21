477074e93bf530c78778bc85a123e7398eae952c
Corina Paulas <corinapaulas.tech@gmail.com>
2020-04-21 22:51:44 +0300

    Code Challenge 1 - Stage 2

--- _1_basics/src/main/java/code/_4_student_effort/Challenge1/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Challenge1/Main.java
@@ -1,5 +1,6 @@
-package code._4_student_effort;
+package code._4_student_effort.Challenge1;
 
+//FizzBuzz - Challenge 1
 public class Main {
 
   public static void main(String[] args) {
@@ -16,6 +17,16 @@ public class Main {
       else if(i%3==0)
       {
         System.out.println("Fizz");
+      }
+
+      if(i%7==0)
+      {
+        System.out.println("Rizz");
+      }
+      else if(i%11==0)
+      {
+
+        System.out.println("Jazz");
       }
       else
         System.out.println(i);
