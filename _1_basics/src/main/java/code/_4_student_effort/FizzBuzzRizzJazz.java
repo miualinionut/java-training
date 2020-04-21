6f68b3079c9ed91a338c59fdbbd9314092d9b2a8
AnastasiaGheorghe <anastasiagabriela79@gmail.com>
2020-04-21 17:39:24 +0300

    Exercitiu FizzBuzzRizzJazz

---
+++ _1_basics/src/main/java/code/_4_student_effort/FizzBuzzRizzJazz.java
@@ -0,0 +1,22 @@
+package code._4_student_effort;
+
+public class FizzBuzzRizzJazz {
+  public static void main(String args[])
+  {
+    for int i=0; i<100; i++)
+    {
+      if (i % 15 == 0)
+        System.out.println("FizzBuzz");
+      else if (i % 3 == 0)
+        System.out.println("Fizz");
+      else if (i % 5 == 0)
+        System.out.println("Buzz");
+      else if (i % 7 == 0)
+        System.out.println("Rizz");
+      else if (i % 11 == 0)
+        System.out.println("Jazz");
+      else System.out.println(i);
+    }
+  }
+}
+
