2defe990c9e66f9bcf4dea378589fe345a647e0d
Corina Paulas <corinapaulas.tech@gmail.com>
2020-04-21 22:34:23 +0300

    Code Challenge 1 - Stage 1

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,6 +3,23 @@ package code._4_student_effort;
 public class Main {
 
   public static void main(String[] args) {
-    //TODO put your code changes in here
+    for(int i=1;i<=100;i++)
+    {
+      if((i%3==0)&&(i%5==0))
+      {
+        System.out.println("FizzBuzz");
+      }
+      else if(i%5==0)
+      {
+        System.out.println("Buzz");
+      }
+      else if(i%3==0)
+      {
+        System.out.println("Fizz");
+      }
+      else
+        System.out.println(i);
+
+    }
   }
 }
