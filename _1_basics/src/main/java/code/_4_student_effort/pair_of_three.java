3c6872901749f40a84e454388d0f43a3687680db
Filip Nechita <filip.nechita@gmail.com>
2020-04-22 16:44:51 +0300

    tema1

--- _1_basics/src/main/java/code/_4_student_effort/pair_of_three.java
+++ _1_basics/src/main/java/code/_4_student_effort/pair_of_three.java
@@ -1,4 +1,25 @@
 package code._4_student_effort;
 
 public class pair_of_three {
+    public static void main(String[] args) {
+        int[] array = {-1, -1, -1, 2};
+        int sum = 0;
+        int contor = 0;
+        int[] v = new int[array.length];
+        for (int i = 0; i < array.length; i++)
+            v[i] = 0;
+        for (int i = 0; i < array.length - 2; i++) {
+            for (int j = i + 1; j < array.length - 1; j++)
+                for (int k = j + 1; k < array.length; k++)
+                    if (array[i] + array[j] + array[k] == sum)
+                        if (v[i] == 0 && v[j] == 0 && v[k] == 0) {
+                            v[i] = 1;
+                            v[j] = 1;
+                            v[k] = 1;
+                            contor++;
+                        }
+        }
+        System.out.println("Numarul de perechi este:" + contor);
+    }
 }
+
--- _1_basics/src/main/java/code/_4_student_effort/pair_of_two.java
+++ _1_basics/src/main/java/code/_4_student_effort/pair_of_two.java
@@ -4,13 +4,21 @@ import java.util.Arrays;
 
 public class pair_of_two {
     public static void main(String[] args) {
-        int[] array = {3, 2, -3, -2, 0};
+        int[] array = {3, 2, -3, -2, 3, 0};
         int sum = 0;
         int contor = 0;
+        int[] v = new int[array.length];
         for (int i = 0; i < array.length; i++)
+            v[i] = 0;
+        for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++)
                 if (array[i] + array[j] == sum)
-                    contor++;
+                    if (v[i] == 0 && v[j] == 0) {
+                        v[i] = 1;
+                        v[j] = 1;
+                        contor++;
+                    }
+        }
         System.out.println("Numarul de perechi este:" + contor);
     }
 }
