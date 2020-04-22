1702772bb788b22d5e624d2ce0bbfb37d34a18ac
AnastasiaGheorghe <anastasiagabriela79@gmail.com>
2020-04-22 22:21:44 +0300

    Curs 2 Java

---
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -0,0 +1,11 @@
+package code._3_in_class;
+
+public class Main{
+    public static void main(String[] args){
+        int i=0;
+        int j=5;
+        int a=4;
+
+        System.out.println(a);
+    }
+}
