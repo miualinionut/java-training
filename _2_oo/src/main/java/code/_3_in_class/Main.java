3f1ba620e9440f544eedca3f85c50918a94fe75a
Sebastian <sebastian.rapa16@gmail.com>
2020-04-22 21:41:19 +0300

    In class Model

--- _2_oo/src/main/java/code/_3_in_class/Main.java
+++ _2_oo/src/main/java/code/_3_in_class/Main.java
@@ -3,6 +3,11 @@ package code._3_in_class;
 public class Main {
 
   public static void main(String[] args) {
-    //TODO put your code changes in here
+    int i = 5;
+
+    Masina m = new Masina();
+    m.start();
+    m.steer();
+    m.stop();
   }
 }
---
+++ _2_oo/src/main/java/code/_3_in_class/Masina.java
@@ -0,0 +1,45 @@
+package code._3_in_class;
+
+public class Masina {
+    // State
+    private final String culoare;
+    private final String marca;
+    private int viteza;
+
+    // Constructor
+    public Masina(String culoare, String marca) {
+        this.culoare = culoare;
+        this.marca = marca;
+    }
+
+    // Getter & Setter
+    public String getCuloare() {
+        return culoare;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getViteza() {
+        return viteza;
+    }
+
+    public void setViteza(int viteza) {
+        this.viteza = viteza;
+    }
+
+    // Behavior
+    void start() {
+        System.out.println("Masina porneste");
+    }
+
+    void stop() {
+        System.out.println("Masina se opreste");
+    }
+
+    void steer() {
+        System.out.println("Masina coteste");
+    }
+
+}
