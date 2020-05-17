cac23b756f363aa234f20b7ab14612846a4edc89
Sebastian <sebastian.rapa16@gmail.com>
2020-05-17 14:47:14 +0300

    Solved Code Challenge 5 Binary Search

---
+++ _5_generics/src/main/java/code/_4_student_effort/code_challenge_5_Binary_Search/ArrayNotSortedException.java
@@ -0,0 +1,12 @@
+package code._4_student_effort.code_challenge_5_Binary_Search;
+
+public class ArrayNotSortedException extends RuntimeException {
+
+    public ArrayNotSortedException(String message) {
+        super(message);
+    }
+
+    public ArrayNotSortedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
