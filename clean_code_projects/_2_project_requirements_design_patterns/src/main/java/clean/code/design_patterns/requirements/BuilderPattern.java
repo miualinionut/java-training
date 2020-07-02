84c046025f674ce3cbbee56e5ee06ffdb78bb806
Miruna Baceanu <mirunabaceanu@yahoo.com>
2020-07-02 23:11:57 +0300

    Proiect_Design_Patterns

---
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/BuilderPattern.java
@@ -0,0 +1,69 @@
+package clean.code.design_patterns.requirements;
+
+public class BuilderPattern {
+
+    static class Apartament {
+        private Apartament(Builder builder) {
+            this.zona = builder.zona;
+            this.balcon = builder.balcon;
+            this.etaj = builder.etaj;
+            this.dimensiune = builder.dimensiune;
+            this.pret = builder.pret;
+        }
+
+        private String zona;
+        private boolean balcon;
+        private int etaj;
+        private int dimensiune;
+        private int pret;
+
+        public static class Builder {
+            private String zona;
+            private boolean balcon;
+            private int etaj;
+            private int dimensiune;
+            private int pret;
+
+            public Builder (String zona) {
+                this.zona = zona;
+            }
+
+            public Builder balcon (boolean val) {
+                balcon = val;
+                return this;
+            }
+
+            public Builder etaj (int val) {
+                etaj = val;
+                return this;
+            }
+
+            public Builder dimensiune (int val) {
+                dimensiune = val;
+                return this;
+            }
+
+            public Builder pret (int val) {
+                pret = val;
+                return this;
+            }
+
+            public Apartament build() {
+                return new Apartament(this);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Apartament [zona=%s, balcon=%s, etaj=%s, dimensiune=%sm^2 pret=%s$]",
+                            zona, balcon, etaj, dimensiune, pret);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Apartament ap = new Apartament.Builder("Pipera").balcon(
+                true).etaj(3).dimensiune(64).pret(150000).build();
+        System.out.println(ap);
+    }
+}
---
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/FactoryPattern.java
@@ -0,0 +1,57 @@
+package clean.code.design_patterns.requirements;
+
+public class FactoryPattern {
+    public static class ColetFactory {
+        public static Colet getColet(String produs,
+                                       String statusLivrare) {
+            if(statusLivrare.equalsIgnoreCase("D")){
+                return new Expediat(produs);
+            }else if(statusLivrare.equalsIgnoreCase("N")){
+                return new Neexpediat(produs);
+            }
+            return null;
+        }
+    }
+
+    static abstract class Colet {
+        Colet(String produs){
+            this.produs = produs;
+        }
+        private String produs;
+        abstract String getMessage();
+        String getProdusAndMessage(){
+            return getMessage() + " " + produs;
+        }
+    }
+
+    static class Expediat extends Colet{
+        public Expediat(String produs) {
+            super(produs);
+        }
+
+        @Override
+        String getMessage() {
+            return "Colet expediat:";
+        }
+
+    }
+
+    static class Neexpediat extends Colet{
+        public Neexpediat(String produs) {
+            super(produs);
+        }
+
+        @Override
+        String getMessage() {
+            return "Colet neexpediat:";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Colet expediat = ColetFactory.getColet("bormasina","D");
+        System.out.println(expediat.getProdusAndMessage());
+        Colet neexpediat = ColetFactory.getColet("scaun de birou","N");
+        System.out.println(neexpediat.getProdusAndMessage());
+    }
+}
