f7cc64400860a1bdd8b731026f48ec66a9ede60c
Tabacitu Bianca <tabacitubianca@gmail.com>
2020-07-09 20:10:40 +0300

    BellTriangleChallenge

---
+++ _6_collections/src/main/java/code/_4_student_effort/BellTriangleChallenge.java
@@ -0,0 +1,31 @@
+package code._4_student_effort;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class BellTriangleChallenge {
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = generate(7);
+        for (List<Integer> row : triangle) {
+            for (Integer cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static List<List<Integer>> generate(int n) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(asList(1));
+        for (int i = 1; i < n; i++) {
+            List<Integer> rowFromAbove = triangle.get(i - 1);
+            List<Integer> currentRow = new ArrayList<>();
+            currentRow.add(rowFromAbove.get(i - 1));
+            for (int j = 1; j <= i; j++) {
+                currentRow.add(currentRow.get(j-1)+ rowFromAbove.get(j-1));
+            }
+            triangle.add(currentRow);
+        }
+        return triangle;
+    }
+}
