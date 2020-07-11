3ed60373b136df0c850b03bb37fd6f9ecad67af4
Tabacitu Bianca <tabacitubianca@gmail.com>
2020-07-11 14:11:15 +0300

    RabbitChallenge

---
+++ _7_concurrency/src/main/java/code/_4_student_effort/RabbitThread.java
@@ -0,0 +1,10 @@
+package code._4_student_effort;
+public class RabbitThread extends Thread {
+    private int nr;
+    public RabbitThread(int nr){
+        this.nr = nr;
+    }
+    public void run(){
+        System.out.printf("Rabbit %s is running \n", nr);
+    }
+}
