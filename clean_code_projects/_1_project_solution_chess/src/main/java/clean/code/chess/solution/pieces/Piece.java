61716d18db11da5a03266e70af6c5946f9849d78
Tabacitu Bianca <tabacitubianca@gmail.com>
2020-07-18 17:29:03 +0300

    MonoChallenge

--- clean_code_projects/_1_project_solution_chess/src/main/java/clean/code/chess/solution/pieces/Piece.java
+++ clean_code_projects/_1_project_solution_chess/src/main/java/clean/code/chess/solution/pieces/Piece.java
@@ -1,5 +1,6 @@
 package clean.code.chess.solution.pieces;
 
+import clean.code.chess.solution.ChessBoard;
 import clean.code.chess.solution.pieces.attributes.Color;
 
 import java.util.logging.Logger;
