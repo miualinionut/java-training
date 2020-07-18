1ac4b56dd9f5ec72dd1c3eec8666d413819857b3
Tabacitu Bianca <tabacitubianca@gmail.com>
2020-07-18 18:53:10 +0300

    TransformationsChallenge

---
+++ _9_reactive/src/main/java/code/_4_student_effort/User.java
@@ -0,0 +1,34 @@
+package code._4_student_effort;
+
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public final String username;
+
+    public User(String firstName, String lastName, String username) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
