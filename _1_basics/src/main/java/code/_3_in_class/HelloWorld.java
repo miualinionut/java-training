20661670b941098f6464041a81ef05fa4f729046
mihaigabriel18 <mihaicalitescu18@gmail.com>
2020-11-16 13:48:19 +0200

    hello world 3

--- _1_basics/src/main/java/code/_3_in_class/HelloWorld.java
+++ _1_basics/src/main/java/code/_3_in_class/HelloWorld.java
@@ -3,6 +3,6 @@ package code._3_in_class;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hello world 2");
+        System.out.println("hello world 3");
     }
 }
