c4bcea725ed78f8cb25455e8711778f68cc8de06
Buhai Darius <buhaidarius@gmail.com>
2020-11-16 20:51:24 +0200

    Update Ex 2

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -2,7 +2,18 @@ package code._4_student_effort;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello world! I'm Darius");
+  private static void checkXValue(int x, int expectedValue) {
+      System.out.println("Have you guessed the value of x correctly? - "  + (x==expectedValue));
   }
+
+  public static void main(String[] args) {
+    // 1
+    System.out.println("Hello world! I'm Darius");
+
+    // 2
+    int x = 0;
+    while(x++ < 10);
+    checkXValue(x, 11);
+  }
+
 }
