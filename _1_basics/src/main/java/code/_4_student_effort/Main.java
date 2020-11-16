877eca149e25d20c8df499b67d000d6948c82dd6
Alex9915 <danielbuzincu31@gmail.com>
2020-11-16 20:35:30 +0200

    Primul mesaj

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,6 +3,7 @@ package code._4_student_effort;
 public class Main {
 
   public static void main(String[] args) {
-    //TODO put your code changes in here
+    System.out.println("Buna ziua lume!!");
+
   }
 }
