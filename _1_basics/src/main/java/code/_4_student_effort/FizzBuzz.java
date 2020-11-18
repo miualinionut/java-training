e4f7e177a15f99c9e70f560307a503dc10410627
torace1 <mimo.mihai14@gmail.com>
2020-11-18 20:16:37 +0200

    stage 2 from fizz buzz

--- _1_basics/src/main/java/code/_4_student_effort/FizzBuzz.java
+++ _1_basics/src/main/java/code/_4_student_effort/FizzBuzz.java
@@ -8,11 +8,32 @@ public class FizzBuzz {
                 if(i%3==0) {
                     System.out.println("FizzBuzz");
                 }
+                else if(i%7==0){
+                    System.out.println("RizzBuzz");
+                }
+                else if(i%11==0){
+                    System.out.println("RizzJazz");
+                }
                 System.out.println("Buzz");
             }
             else if(i%3==0){
+                 if(i%7==0){
+                     System.out.println("FizzRizz");
+                 }
+                 else if(i%11==0){
+                     System.out.println("FizzJazz");
+                 }
                 System.out.println("Fizz");
             }
+             else if(i%7==0){
+                 if(i%11==0){
+                     System.out.println("RizzJazz");
+                 }
+                 System.out.println("Rizz");
+             }
+             else if(i%11==0){
+                 System.out.println("Jazz");
+             }
             else
                 System.out.println(i);
         }
