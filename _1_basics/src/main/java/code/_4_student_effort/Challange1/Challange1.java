fd3cf90c71cef29d94c300554c8ce5d9908e8b39
Mihai Fleser <mihaifleser@yahoo.com>
2020-11-18 19:51:31 +0200

    Challange 1 code update

--- _1_basics/src/main/java/code/_4_student_effort/Challange1/Challange1.java
+++ _1_basics/src/main/java/code/_4_student_effort/Challange1/Challange1.java
@@ -14,7 +14,7 @@ public class Challange1 {
                 System.out.print(i + " ");
         }
         System.out.println("");
-
+        System.out.println("");
         for(int i = 1; i <= 100; i++)
         {
             if(i % 7 == 0)
