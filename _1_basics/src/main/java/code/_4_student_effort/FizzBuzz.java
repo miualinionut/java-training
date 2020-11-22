896854e4f2c9d25b369be1c0a22cc3bda84d382d
Ema_Arsene <ema.arsene4@gmail.com>
2020-11-22 15:13:49 +0200

    fizz buzz implementation

--- _1_basics/src/main/java/code/_4_student_effort/FizzBuzz.java
+++ _1_basics/src/main/java/code/_4_student_effort/FizzBuzz.java
@@ -7,25 +7,28 @@ public class FizzBuzz {
     public void FizzBuzz () {
         for (int i = 1; i <= 100; i++)
         {
-            // Stage 1
-            if ((i % 3 == 0) && (i % 5 !=0)) //divisible by 3 & not divisible by 5
+            if ((i % 3 == 0) && (i % 5 != 0) && (i % 7 != 0) && (i % 11 != 0)) //divisible only by 3
                 System.out.print("Fizz, ");
-            else if ((i % 5 == 0) && (i % 3 !=0)) //divisible by 5 & not divisible by 3
+            else if ((i % 5 == 0) && (i % 3 != 0) && (i % 7 != 0) && (i % 11 != 0)) //div only by 5
                 System.out.print("Buzz, ");
-            else if (i % 15 == 0) //divisible by 15 <=> div by 3 AND 5
-                System.out.println("FizzBUzz ");
-
-            //Stage 2
-            else if ((i % 7 == 0) && (i % 11 !=0))
+            else if ((i % 7 == 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 11 != 0)) //div only by 7
                 System.out.print("Rizz, ");
-            else if ((i % 11 == 0) && (i % 7 !=0))
+            else if ((i % 11 == 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 77 != 0)) //div only by 11
                 System.out.print("Jazz, ");
-            else if (i % 7 == 0 && i % 11 == 0)
-                System.out.print("RizzJazz, ");
+            else if ((i % 15 == 0)&& (i % 7 != 0) && (i % 11 != 0)) //div only by 3 AND 5
+                System.out.println("FizzBuzz, ");
+            else if ((i % 21 == 0)&& (i % 5 != 0) && (i % 11 != 0)) //div only by 3 AND 7
+                System.out.println("FizzRizz, ");
+            else if ((i % 33 == 0)&& (i % 5 != 0) && (i % 7 != 0)) //div only by 3 AND 11
+                System.out.println("FizzJazz, ");
+            else if ((i % 35 == 0)&& (i % 3 != 0) && (i % 11 != 0)) //div only by 5 AND 7
+                System.out.println("BuzzRizz, ");
+            else if ((i % 55 == 0)&& (i % 3 != 0) && (i % 7 != 0)) //div only by 5 AND 11
+                System.out.println("BuzzJazz, ");
+            else if ((i % 77 == 0)&& (i % 3 != 0) && (i % 5 != 0)) //div only by 7 AND 11
+                System.out.println("RizzJazz, ");
             else
-                System.out.print(i + ", ");
-            /*It wasn't specified what happens when the number is divisible by both 7 and 11 (e.g 77 in range 1 - 100)
-            so i assumed we should follow the FizzBuzz Pattern */
+                System.out.print(i + ", "); //default case
         }
     }
 }
