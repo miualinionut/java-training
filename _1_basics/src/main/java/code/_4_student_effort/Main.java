6233b0ce9713219a1c157b6646b3717b29734c65
Anda <alexandrescu.anda22@gmail.com>
2020-11-22 15:37:12 +0200

    FizzBuzz challenge

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,6 +3,24 @@ package code._4_student_effort;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Buna ziua lume!");
+    FizzBuzz(100);
+
+  }
+
+  private static void FizzBuzz(int n){
+    for(int i=1; i<n; i=i+1)
+    {
+     if(((i % 3) == 0) && ((i % 5) == 0) && ((i % 7) != 0) && ((i % 11) != 0))
+       System.out.println("FizzBuzz");
+     else if(((i % 3) == 0) && ((i % 7) != 0) && ((i % 11) != 0))
+       System.out.println("Fizz");
+     else if(((i % 5) == 0) && ((i % 7) != 0) && ((i % 11) != 0))
+       System.out.println("Buzz");
+     else if(((i % 7) == 0) && ((i % 11) != 0))
+       System.out.println("Rizz");
+     else if(((i % 7) != 0) && ((i % 11) == 0))
+       System.out.println("Jazz");
+     else System.out.println(i);
+    }
   }
 }
