61fee2f39db6ccefdb6db56ca5e068c78f4535fd
Sarbu Ioan <sarbu.ioan.vladimir@gmail.com>
2020-11-23 16:31:10 +0200

    FizzBuzz corectat

--- _1_basics/src/main/java/code/_4_student_effort/FizzBuzz.java
+++ _1_basics/src/main/java/code/_4_student_effort/FizzBuzz.java
@@ -3,29 +3,31 @@ package code._4_student_effort;
 public class FizzBuzz {
     public static void main(String[] args)
     {
-        for(int nr = 1; nr <= 100; nr++)
+
+        for(int nr = 1; nr <= 110; nr++)
         {
+            boolean divizibil = false;
             if(nr % 3 == 0)
             {
                 System.out.print("Fizz");
-                if(nr % 5 == 0)
-                {
-                    System.out.print("Buzz");
-                }
+                divizibil = true;
             }
-            else if(nr % 5 == 0)
+            if(nr % 5 == 0)
             {
                 System.out.print("Buzz");
+                divizibil = true;
             }
-            else if(nr % 7 == 0) //Stage 2
+            if(nr % 7 == 0) //Stage 2
             {
                 System.out.print("Rizz");
+                divizibil = true;
             }
-            else if(nr % 11 == 0)
+            if(nr % 11 == 0)
             {
                 System.out.print("Jazz");
+                divizibil = true;
             }
-            else
+            if(!divizibil)
             {
                 System.out.print(nr);
             }
