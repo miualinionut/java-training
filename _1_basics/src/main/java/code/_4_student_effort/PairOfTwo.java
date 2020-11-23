e8582b7b4d5e2aaa6de58b8b41072930105752bb
Maria <mar1asm@yahoo.com>
2020-11-23 17:01:46 +0200

    pair of 2 -update

--- _1_basics/src/main/java/code/_4_student_effort/PairOfTwo.java
+++ _1_basics/src/main/java/code/_4_student_effort/PairOfTwo.java
@@ -1,4 +1,17 @@
 package code._4_student_effort;
-
+import java.util.HashMap;
 public class PairOfTwo {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> values = new HashMap<Integer, Integer>();
+        int v[]={3,2,-3,-3,-2,0,1,3};
+        int pairs =0;
+        for (int n:v){
+            if (values.containsKey(-n)){
+                pairs++;
+            }else {
+                values.put(n, 1);
+            }
+        }
+        System.out.println(pairs);
+    }
 }
