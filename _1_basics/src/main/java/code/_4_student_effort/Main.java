9843eb15dc180ec6db87ccc4c8bd44c847db0e26
Alexandru Bojog <alexbojog@gmail.com>
2020-11-23 13:14:58 +0200

    Changing the print in english.

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,6 +3,6 @@ package code._4_student_effort;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Buna ziua lume!");
+    System.out.println("Hello World!");
   }
 }
