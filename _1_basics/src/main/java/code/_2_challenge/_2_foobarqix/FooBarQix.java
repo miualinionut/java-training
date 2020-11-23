f37aeaf55d7ace66ff052fac195d0144918608b4
Maria <mar1asm@yahoo.com>
2020-11-23 16:39:33 +0200

    Revert "update"
    This reverts commit 7c9bdbf3601a433de1bc25985b388cbeb59cd0b1.

---
+++ _1_basics/src/main/java/code/_2_challenge/_2_foobarqix/FooBarQix.java
@@ -0,0 +1,58 @@
+package code._2_challenge._2_foobarqix;
+
+public class FooBarQix {
+  public String compute(int i) {
+    String s = "";
+    if (i % 3 == 0) s += "Foo";
+    if (i % 5 == 0) s += "Bar";
+    if (i % 7 == 0) s += "Qix";
+
+    char[] digits = String.valueOf(i).toCharArray();
+    for (char digit : digits) {
+
+      if (digit == '3') s += "Foo";
+      if (digit == '5') s += "Bar";
+      if (digit == '7') s += "Qix";
+
+    }
+
+    if (s.isEmpty()) {
+      s = String.valueOf(i);
+    }
+    return s;
+  }
+
+  public static String compute2(int i) {
+    String s = "";
+
+    boolean isDivisible = false;
+    if (i % 3 == 0) {
+      s += "Foo";
+      isDivisible = true;
+    }
+    if (i % 5 == 0) {
+      s += "Bar";
+      isDivisible = true;
+    }
+    if (i % 7 == 0) {
+      s += "Qix";
+      isDivisible = true;
+    }
+
+    char[] digits = String.valueOf(i).toCharArray();
+    for (char digit : digits) {
+
+      if (digit == '3') s += "Foo";
+      else if (digit == '5') s += "Bar";
+      else if (digit == '7') s += "Qix";
+      else if (digit == '0') s += "*";
+      else if (!isDivisible) s += digit;
+    }
+
+    return s;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(compute2(101));
+  }
+}
