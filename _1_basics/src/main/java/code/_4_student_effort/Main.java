e2ee3574b3e7ad9d3b730758bac64426b578611d
dianaboros <dianaboros29@gmail.com>
2020-11-23 15:37:04 +0200

    Pair of 2 implementation

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -6,11 +6,12 @@ public class Main {
     System.out.println("hello");
 
     //PairOf2
-    int[] numbers = {1,1,0,-1,-1};
-    System.out.println(PairOf2.checkPairs(numbers));
+    int[] numbers = {5,9,-5,7,-5};
+    System.out.println(PairOf2.countPairs(numbers));
 
     //FooBarQix
     System.out.println(FooBarQix.compute(15));
     System.out.println(FooBarQix.compute2(105));
+
   }
 }
--- _1_basics/src/main/java/code/_4_student_effort/PairOf2.java
+++ _1_basics/src/main/java/code/_4_student_effort/PairOf2.java
@@ -1,16 +1,37 @@
 package code._4_student_effort;
 
+import java.util.Arrays;
+
 public class PairOf2 {
-   public static int checkPairs(int[] numbers){
+   public static int countPairs(int[] numbers){
     int count = 0;
-    for (int i =0; i< numbers.length; i++){
-        for (int j=i; j< numbers.length; j++){
-            if ((numbers[i] != numbers[j]) && (numbers[i] + numbers[j] == 0)) {
-            count++;
-            numbers[j] = 0;
-            break;}
-            }
+    int i = 0;
+    int len = numbers.length-1;
+    sortArray(numbers);
+    while(i<len){
+        if(numbers[i]+numbers[len]>0){
+            len--;
         }
+        else if (numbers[i]+numbers[len]<0){
+            i++;
+        }
+        else if (numbers[i]+numbers[len]==0) {
+            count++;
+            len--;
+            i++;}
+    }
     return count;
     }
-   }
+
+    private static void sortArray(int[] numbers){
+       int temp;
+       for(int i = 0; i< numbers.length-1; i++){
+           for (int j = i+1; j<numbers.length; j++){
+               if(numbers[i] > numbers[j]){
+               temp = numbers[i];
+               numbers[i] = numbers[j];
+               numbers[j] = temp;}
+           }
+       }
+    }
+}
