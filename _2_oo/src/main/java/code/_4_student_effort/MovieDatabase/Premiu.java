71605ed9f6eae22365060df3b8f91366594078af
roberpgu <contact.robert00@gmail.com>
2020-11-28 17:03:54 +0200

    MovieDatabase

---
+++ _2_oo/src/main/java/code/_4_student_effort/MovieDatabase/Premiu.java
@@ -0,0 +1,13 @@
+package code._4_student_effort.MovieDatabase;
+
+public class Premiu {
+    public String nume;
+    public Integer an;
+
+    public Premiu(String nume,Integer an){
+        this.nume = nume;
+        this.an = an;
+    }
+
+
+}
