c40845d0b492b1603c9356eff44680a6102133cc
andreeabarbu1 <andreeabarbu169@gmail.com>
2020-11-29 00:43:57 +0200

    Fighter

---
+++ _2_oo/src/main/java/code/_4_student_effort/BoxingMatch.java
@@ -0,0 +1,32 @@
+package code._4_student_effort;
+
+public class BoxingMatch {
+    private Fighter firstFighter;
+    private Fighter secondFighter;
+
+    public BoxingMatch (Fighter firstFighter, Fighter secondFighter) {
+        this.firstFighter = firstFighter;
+        this.secondFighter = secondFighter;
+    }
+
+    public String fight() {
+        if (firstFighter.getHealth() >= 0 && secondFighter.getHealth() >= 0) {
+            firstFighter.attack(secondFighter);
+            secondFighter.attack(firstFighter);
+        }
+
+        if (firstFighter.getHealth() > secondFighter.getHealth()) {
+            return firstFighter.getName();
+        } else {
+            return secondFighter.getName();
+        }
+    }
+
+    public static void main(String[] args) {
+        Fighter firstFighter = new Fighter("First", 100, 7);
+        Fighter secondFighter = new Fighter("Second", 100, 13);
+
+        BoxingMatch match = new BoxingMatch(firstFighter, secondFighter);
+        System.out.println(match.fight());
+    }
+}
---
+++ _2_oo/src/main/java/code/_4_student_effort/Fighter.java
@@ -0,0 +1,25 @@
+package code._4_student_effort;
+
+public class Fighter {
+    private String name;
+    private int health;
+    private int damagePerAttack;
+
+    public Fighter (String name, int health, int damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attack(Fighter opponent) {
+        opponent.health =- this.damagePerAttack;
+    }
+}
