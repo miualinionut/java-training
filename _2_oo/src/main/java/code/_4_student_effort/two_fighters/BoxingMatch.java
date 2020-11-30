cacf2d67cdd33a84f7550b10658f0ad9e8fe1544
Maria <mar1asm@yahoo.com>
2020-11-30 18:46:57 +0200

    Update BoxingMatch.java
    Fara recursivitate

--- _2_oo/src/main/java/code/_4_student_effort/two_fighters/BoxingMatch.java
+++ _2_oo/src/main/java/code/_4_student_effort/two_fighters/BoxingMatch.java
@@ -7,12 +7,19 @@ public class BoxingMatch {
         this.f2=f2;
     }
     public String fight(){
-        f1.attack(f2);
-        if (f2.health == 0)
-            return f1.name;
-        f2.attack(f1);
-        if (f1.health == 0)
-            return f2.name;
-        return (fight());
+        String fighterName = "";
+        if (f1.damagePerAttack == 0 && f2.damagePerAttack ==0)
+            return "nu se poate";
+        while (f1.health > 0 && f2.health > 0){
+            f1.attack(f2);
+            if (f2.health == 0) {
+                fighterName = f1.name;
+                break;
+            }
+            f2.attack(f1);
+            if (f1.health == 0)
+                fighterName = f2.name;
+        }
+        return fighterName;
     }
 }
