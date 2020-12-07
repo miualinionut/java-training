87c1e55e4185c55d7742cb60d088fbe20cb286e9
stanescu-alex007 <alex.stanescu86@yahoo.com>
2020-12-07 21:22:00 +0200

    Challenge 6 Builder Update

--- _1_basics/src/main/java/code/_4_student_effort/Show.java
+++ _1_basics/src/main/java/code/_4_student_effort/Show.java
@@ -2,8 +2,12 @@ package code._4_student_effort;
 
 public class Show {
     public static void main(String[] args) {
-        Information person = new Information.Builder(/*name, job, etc*/).build();
-
+       Information person = new Builder("Alex")
+               .setJob("Developer")
+               .setDrivingLicense(true)
+               .setIsMarried(false)
+               .build();
+        System.out.println(person.getJob());
     }
 
 }
