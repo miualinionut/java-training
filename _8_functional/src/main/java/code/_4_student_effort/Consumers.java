32f50fd31f210cd6544977ba4376de10f0cb9839
dianaboros <dianaboros29@gmail.com>
2021-01-18 17:01:19 +0200

    consumer

---
+++ _8_functional/src/main/java/code/_4_student_effort/Consumers.java
@@ -0,0 +1,35 @@
+package code._4_student_effort;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class Consumers {
+    @Test
+    public void consumer_1(){
+        List<String> list = new ArrayList<>(Arrays.asList("a","b","c"));
+
+        Consumer<List<String>> consumer = List::clear;
+        consumer.accept(list);
+        Assert.assertEquals(list, new ArrayList<>());
+    }
+
+    @Test
+    public void cosumer_2(){
+        List<String> list1 = new ArrayList<>(Arrays.asList("a","b","c"));
+        List<String> list2 = new ArrayList<>(Arrays.asList("a","b","c","first","second"));
+        Consumer<List<String>> c1 = list ->list.add("first");
+        Consumer<List<String>> c2 = list -> list.add("second");
+
+        Consumer<List<String>> consumer = c1.andThen(c2);
+        consumer.accept(list1);
+
+        Assert.assertEquals(list1,list2);
+    }
+}
