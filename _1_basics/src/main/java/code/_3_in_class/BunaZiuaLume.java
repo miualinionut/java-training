78faac871569cfcce6335a7834618e9054390614
vladutanuandrei <vladutanuandrei@gmail.com>
2021-05-10 21:38:21 +0300

    Vlad-Andrei Mihai curs 1 - commit 2

--- _1_basics/src/main/java/code/_3_in_class/BunaZiuaLume.java
+++ _1_basics/src/main/java/code/_3_in_class/BunaZiuaLume.java
@@ -3,5 +3,19 @@ package code._3_in_class;
 public class BunaZiuaLume {
     public static void main(String[] args) {
         System.out.println("Buna ziua lume");
+        System.out.println(true || false); //cand punem cu 2, se opreste verificarea la prima conditie true
+        System.out.println(true | false);   //se apeleaza toate conditiile
+        System.out.println("-------------------");
+        System.out.println(m1()||m2());
+        System.out.println("-------------------");
+        System.out.println(m1()|m2());
+    }
+    public static boolean m1(){
+        System.out.println("s-a apelat m1");
+        return true;
+    }
+    public static boolean m2(){
+        System.out.println("s-a apelat m2");
+        return false;
     }
 }
