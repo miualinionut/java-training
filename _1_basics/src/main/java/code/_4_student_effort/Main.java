1ea5907d40da9e05db175d04e71a29a8a51c5218
andreeaiulia <andreeaiulia.albu@gmail.com>
2021-05-10 20:09:59 +0300

    Second try - Andreea Albu

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,7 +3,7 @@ package code._4_student_effort;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Changes");
+        System.out.println("Changes1");
         //TODO put your code changes in here
     }
 }
