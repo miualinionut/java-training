9e5e39fe87f686e4079f8341dd4e400e6d6f0928
andreeaiulia <andreeaiulia.albu@gmail.com>
2021-05-10 20:02:57 +0300

    First commit in java project - Andreea Albu

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,6 +3,7 @@ package code._4_student_effort;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Changes");
         //TODO put your code changes in here
     }
 }
