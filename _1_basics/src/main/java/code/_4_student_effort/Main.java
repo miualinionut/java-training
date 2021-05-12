ca5c3f1c96b64a7dc76a676cccb5d9616938bb52
ionut <ionut.marginean01@gmail.com>
2021-05-12 18:13:17 +0300

    Code Challenge 1

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -2,7 +2,21 @@ package code._4_student_effort;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
         //TODO put your code changes in here
+
+        for (int i = 1; i < 101; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.print("FizzBuzz ");
+            }else if (i % 3 == 0){
+                System.out.print("Fizz ");
+            }else if (i % 5 == 0){
+                System.out.print("Buzz ");
+            }else {
+                System.out.print(" "+ i +" ");
+            }
+        }
     }
 }
