d5716ab60de17c699038da96de23bfcb67e15fd9
ionut <ionut.marginean01@gmail.com>
2021-05-14 07:59:58 +0300

    Second commit

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -1,22 +1,32 @@
 package code._4_student_effort;
 
+import java.security.PublicKey;
+
 public class Main {
-
-
 
     public static void main(String[] args) {
         //TODO put your code changes in here
 
         for (int i = 1; i < 101; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)){
+            if ((i % 3 == 0) && (i % 5 == 0))
                 System.out.print("FizzBuzz ");
-            }else if (i % 3 == 0){
+
+            else if (i % 3 == 0)
                 System.out.print("Fizz ");
-            }else if (i % 5 == 0){
+
+            else if (i % 5 == 0)
                 System.out.print("Buzz ");
-            }else {
+
+            else if (i % 7 == 0)
+                System.out.print("Rizz ");
+
+            else if(i % 11 == 0)
+                System.out.print("Jazz");
+
+            else
                 System.out.print(" "+ i +" ");
-            }
+
+        }
         }
     }
-}
+
