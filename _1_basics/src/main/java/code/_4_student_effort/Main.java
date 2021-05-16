14b910665b2bb0e142d506eceb02821b600ccb80
stefanstg <stefanstg@yahoo.com>
2021-05-16 17:28:07 +0300

    Pairof3 Stage 1 - GROSU STEFAN

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -6,19 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
         //TODO put your code changes in here
-        int[] arr = new int[]{ 1,-1,1,-1 };
-        int k=0;
+        int[] arr = new int[]{ -1,-1,2,-1 };
+        int l=0;
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
-                if(arr[i] + arr[j] == 0 && arr[j]!=1000){
-                    //arr[i] = 1000;
-                    arr[j] = 1000;
-                    k++;
-                    break;
+                for(int k = j+1; k<arr.length; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0 && arr[j] != 1000 && arr[k] != 1000) {
+                        arr[j] = 1000;
+                        arr[k] = 1000;
+                        l++;
+                        break;
+                    }
                 }
             }
         }
-        System.out.println(k);
+        System.out.println(l);
 
 
     }
