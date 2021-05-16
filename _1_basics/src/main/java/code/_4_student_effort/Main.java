d85720e2017013d151f27cdd56773c8d5476a926
Gabriel Moise <gabrielmoise74@gmail.com>
2021-05-16 16:16:08 +0300

    Code challenge 1 stage 1 Moise Gabriel

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         //TODO put your code changes in here
+        boolean printnumber;
+        for (int i = 1; i <= 100; ++i) {
+            printnumber = true;
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                printnumber = false;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                printnumber = false;
+            }
+            if (printnumber) {
+                System.out.print(i);
+            }
+            System.out.print(", ");
+        }
     }
 }
