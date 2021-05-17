24e2ac010aff11e6285c51454ad39aceee4da9b3
Ana-Maria <anamaria_cusco@yahoo.com>
2021-05-17 17:57:10 +0300

    Challenge 1 and Challenge 2 Week1

---
+++ _1_basics/src/main/java/code/_4_student_effort/MyFizzBuzz.java
@@ -0,0 +1,11 @@
+package code._4_student_effort;
+
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+            for(int i=1;i<=100;i++){
+                if(i%)
+            }
+    }
+
+}
