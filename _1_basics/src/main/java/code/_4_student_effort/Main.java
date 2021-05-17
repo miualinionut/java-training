7e98b5508907ef087afd148dd8d97947db1dae99
ionut <ionut.marginean01@gmail.com>
2021-05-17 08:24:02 +0300

    Challenge 2

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -4,29 +4,33 @@ import java.security.PublicKey;
 
 public class Main {
 
+    String result="";
+
+    public String compute1(int input){
+        if(input % 3 == 0 && input == 3){
+            result = "FooFoo";
+        }else if(input % 3 == 0)
+            return result = "Foo";
+
+        if(input % 5 == 0 && input == 5){
+            result = "BarBar";
+        }else if(input % 5 == 0)
+            return result = "Bar";
+
+        if(input % 7 == 0 && input == 7){
+            result = "QixQix";
+        }else if(input % 7 == 0)
+            return result = "Qix";
+
+        return result;
+    }
+
+
+
     public static void main(String[] args) {
         //TODO put your code changes in here
-
-        for (int i = 1; i < 101; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0))
-                System.out.print("FizzBuzz ");
-
-            else if (i % 3 == 0)
-                System.out.print("Fizz ");
-
-            else if (i % 5 == 0)
-                System.out.print("Buzz ");
-
-            else if (i % 7 == 0)
-                System.out.print("Rizz ");
-
-            else if(i % 11 == 0)
-                System.out.print("Jazz");
-
-            else
-                System.out.print(" "+ i +" ");
-
-        }
-        }
+    Main main = new Main();
+        System.out.println(main.compute1(15));
     }
+}
 
