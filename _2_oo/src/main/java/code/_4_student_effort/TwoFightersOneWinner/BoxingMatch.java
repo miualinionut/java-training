a3997526add7d8daf5488bd9df068c495c3cb7e6
Dragos Anca <anca.dragos.00@gmail.com>
2021-05-24 00:30:37 +0300

    Added BoxingMatch class which takes 2 fighters and makes them fight to death

---
+++ _2_oo/src/main/java/code/_4_student_effort/TwoFightersOneWinner/BoxingMatch.java
@@ -0,0 +1,11 @@
+package code._4_student_effort.TwoFightersOneWinner;
+
+
+
+public class BoxingMatch {
+
+    public static void main(String[] args) {
+        BoxingArena boxingArena = new BoxingArena();
+        boxingArena.run();
+    }
+}
