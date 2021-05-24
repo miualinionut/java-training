b2a7ed2061a7c44ef314d7b5af29c5691d4f7243
ionut <ionut.marginean01@gmail.com>
2021-05-24 08:59:06 +0300

    Week2 Challenge 2

--- _2_oo/src/main/java/code/_4_student_effort/Actor.java
+++ _2_oo/src/main/java/code/_4_student_effort/Actor.java
@@ -1,2 +1,48 @@
-package code._4_student_effort;public class Actor {
+package code._4_student_effort;
+
+import java.util.Arrays;
+
+public class Actor {
+    private String nume;
+    private int varsta;
+    private Premiu[] premiu;
+
+    public Actor(String nume, int varsta, Premiu[] premiu) {
+        this.nume = nume;
+        this.varsta = varsta;
+        this.premiu = premiu;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public int getVarsta() {
+        return varsta;
+    }
+
+    public void setVarsta(int varsta) {
+        this.varsta = varsta;
+    }
+
+    public Premiu[] getPremiu() {
+        return premiu;
+    }
+
+    public void setPremiu(Premiu[] premiu) {
+        this.premiu = premiu;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "nume='" + nume + '\'' +
+                ", varsta=" + varsta +
+                ", premiu=" + Arrays.toString(premiu) +
+                '}';
+    }
 }
