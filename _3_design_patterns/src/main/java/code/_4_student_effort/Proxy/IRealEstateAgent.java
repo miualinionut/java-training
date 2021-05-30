2edfe95439037471116186bb3c046e760ba2a528
Dragos Anca <anca.dragos.00@gmail.com>
2021-05-31 02:52:57 +0300

    Code Challange 3

---
+++ _3_design_patterns/src/main/java/code/_4_student_effort/Proxy/IRealEstateAgent.java
@@ -0,0 +1,6 @@
+package code._4_student_effort.Proxy;
+
+public interface IRealEstateAgent {
+    void represent(Apartament apartament);
+    Apartament rent(Student student);
+}
---
+++ _3_design_patterns/src/main/java/code/_4_student_effort/Proxy/Student.java
@@ -0,0 +1,11 @@
+package code._4_student_effort.Proxy;
+
+public class Student {
+    String nameStudent;
+    int money;
+
+    public Student(String nameStudent, int money) {
+        this.nameStudent = nameStudent;
+        this.money = money;
+    }
+}
