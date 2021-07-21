f90493a4b5d532c1420bfe5ee3124c64da3389c4
fane5115 <fanef92@gmail.com>
2021-07-21 18:13:44 +0300

    CHESS PROJECT

--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
@@ -1,11 +1,13 @@
 package clean.code.chess.requirements;
 
+import java.awt.*;
+
 public class ChessBoard {
 
     public static int MAX_BOARD_WIDTH = 7;
     public static int MAX_BOARD_HEIGHT = 7;
 
-    private Pawn[][] pieces;
+    private final Pawn[][] pieces;
 
     public ChessBoard() {
         pieces = new Pawn[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT];
@@ -13,10 +15,31 @@ public class ChessBoard {
     }
 
     public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+        if (!IsLegalBoardPosition(xCoordinate, yCoordinate)) {
+            pawn.setXCoordinate(-1);
+            pawn.setYCoordinate(-1);
+            return;
+        }
+
+        pawn.setXCoordinate(xCoordinate);
+        pawn.setYCoordinate(yCoordinate);
+        pawn.setPieceColor(pieceColor);
+        pieces[xCoordinate][yCoordinate] = pawn;
     }
 
     public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        if( xCoordinate >= 7 || xCoordinate < 0 )
+            return false;
+        if( yCoordinate >= 7 || yCoordinate < 0 )
+            return false;
+        return pieces[xCoordinate][yCoordinate] == null;
+    }
+
+    public boolean IsNotEmptyPosition(int xCoordinate, int yCoordinate) {
+        if (xCoordinate >= 7 || xCoordinate < 0)
+            return false;
+        if (yCoordinate >= 7 || yCoordinate < 0)
+            return false;
+        return pieces[xCoordinate][yCoordinate] != null;
     }
 }
--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/Pawn.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/Pawn.java
@@ -39,12 +39,41 @@ public class Pawn {
         return this.pieceColor;
     }
 
-    private void setPieceColor(PieceColor value) {
+    public void setPieceColor(PieceColor value) {
         pieceColor = value;
     }
 
     public void Move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
+        if (movementType == MovementType.MOVE) {
+            if (xCoordinate != newX) return;
+            if (!chessBoard.IsLegalBoardPosition(newX, newY)) return;
+
+            if (pieceColor.equals(PieceColor.WHITE)) {
+                if (newY - yCoordinate == 1)
+                    yCoordinate = newY;
+                else if (newY - yCoordinate == 2 && yCoordinate == 1)
+                    yCoordinate = newY;
+            } else {
+                if (newY - yCoordinate == -1)
+                    yCoordinate = newY;
+                else if (newY - yCoordinate == -2 && yCoordinate == 5)
+                    yCoordinate = newY;
+            }
+        } else {
+            if (chessBoard.IsNotEmptyPosition(newX, newY)) {
+                if (pieceColor == PieceColor.WHITE) {
+                    if ((newX == xCoordinate + 1 || newX == xCoordinate - 1) && newY == yCoordinate + 1) {
+                        xCoordinate = newX;
+                        yCoordinate = newY;
+                    }
+                } else {
+                    if ((newX == xCoordinate + 1 || newX == xCoordinate - 1) && newY == yCoordinate - 1) {
+                        xCoordinate = newX;
+                        yCoordinate = newY;
+                    }
+                }
+            }
+        }
     }
 
     @Override
