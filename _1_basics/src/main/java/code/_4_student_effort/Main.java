e7a436c982ea0ee5d890d52c967231f77a540968
MargineanIonut <ionut.marginean01@gmail.com>
2021-08-01 21:27:20 +0200

    changes

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -1,5 +1,7 @@
 package code._4_student_effort;
 
+import code._2_challenge._3_pair_of_2.PairOf2;
+
 import java.security.PublicKey;
 
 public class Main {
@@ -7,26 +9,8 @@ public class Main {
     public static void main(String[] args) {
         //TODO put your code changes in here
 
-        for (int i = 1; i < 101; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0))
-                System.out.print("FizzBuzz ");
-
-            else if (i % 3 == 0)
-                System.out.print("Fizz ");
-
-            else if (i % 5 == 0)
-                System.out.print("Buzz ");
-
-            else if (i % 7 == 0)
-                System.out.print("Rizz ");
-
-            else if(i % 11 == 0)
-                System.out.print("Jazz");
-
-            else
-                System.out.print(" "+ i +" ");
-
-        }
         }
     }
+
+
 
--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
@@ -4,6 +4,7 @@ public class ChessBoard {
 
     public static int MAX_BOARD_WIDTH = 7;
     public static int MAX_BOARD_HEIGHT = 7;
+    boolean result = true;
 
     private Pawn[][] pieces;
 
@@ -13,10 +14,19 @@ public class ChessBoard {
     }
 
     public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+        if ((pieceColor == PieceColor.WHITE) && (xCoordinate == 6) && (yCoordinate <= 7)){
+
+        }else if (((pieceColor == PieceColor.BLACK) && (xCoordinate == 1) && (yCoordinate <= 7))){
+            pieces = new Pawn[xCoordinate][yCoordinate];
+        }else {
+            System.out.println("This is not a valid place for the pawn");
+        }
     }
 
     public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+     if ((xCoordinate <= MAX_BOARD_WIDTH && xCoordinate >= 0) && (yCoordinate <= MAX_BOARD_HEIGHT && yCoordinate >= 0)){
+         return result = true;
+     }else
+         return result = false;
     }
 }
