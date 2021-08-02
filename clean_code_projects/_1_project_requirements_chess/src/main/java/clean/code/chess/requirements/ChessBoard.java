cd3d0a3bde2d79a9c5eb89d20a43f0031cd1622e
ionut_marginean <ionut.marginean@delti.com>
2021-08-02 17:05:35 +0300

    second com

--- clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
+++ clean_code_projects/_1_project_requirements_chess/src/main/java/clean/code/chess/requirements/ChessBoard.java
@@ -5,28 +5,67 @@ public class ChessBoard {
     public static int MAX_BOARD_WIDTH = 7;
     public static int MAX_BOARD_HEIGHT = 7;
     boolean result = true;
-
+    public ChessBoard[][] board;
     private Pawn[][] pieces;
 
     public ChessBoard() {
-        pieces = new Pawn[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT];
-
+        board = new ChessBoard[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT];
     }
 
     public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
         if ((pieceColor == PieceColor.WHITE) && (xCoordinate == 6) && (yCoordinate <= 7)){
 
         }else if (((pieceColor == PieceColor.BLACK) && (xCoordinate == 1) && (yCoordinate <= 7))){
-            pieces = new Pawn[xCoordinate][yCoordinate];
+
         }else {
             System.out.println("This is not a valid place for the pawn");
         }
     }
 
     public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
-     if ((xCoordinate <= MAX_BOARD_WIDTH && xCoordinate >= 0) && (yCoordinate <= MAX_BOARD_HEIGHT && yCoordinate >= 0)){
-         return result = true;
-     }else
-         return result = false;
+        if ((xCoordinate <= MAX_BOARD_WIDTH && xCoordinate >= 0) && (yCoordinate <= MAX_BOARD_HEIGHT && yCoordinate >= 0)){
+            return result = true;
+        }else
+            return result = false;
+    }
+
+    public static int getMaxBoardWidth() {
+        return MAX_BOARD_WIDTH;
+    }
+
+    public static void setMaxBoardWidth(int maxBoardWidth) {
+        MAX_BOARD_WIDTH = maxBoardWidth;
+    }
+
+    public static int getMaxBoardHeight() {
+        return MAX_BOARD_HEIGHT;
+    }
+
+    public static void setMaxBoardHeight(int maxBoardHeight) {
+        MAX_BOARD_HEIGHT = maxBoardHeight;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public ChessBoard[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(ChessBoard[][] board) {
+        this.board = board;
+    }
+
+    public Pawn[][] getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(Pawn[][] pieces) {
+        this.pieces = pieces;
     }
 }
