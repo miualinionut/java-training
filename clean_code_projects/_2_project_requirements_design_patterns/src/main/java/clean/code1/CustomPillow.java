f394d75d9720b1223781720414609e5d36a2df4e
diana_danuta <danuta.diana01@gmail.com>
2021-08-09 20:45:45 +0300

    CheesBoard

--- clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code1/CustomPillow.java
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code1/CustomPillow.java
@@ -1,2 +1,58 @@
-package clean.code1;public class CustomPillow {
+package clean.code1;
+
+public class CustomPillow {
+    private String name;
+    private String color;
+    private int weight;
+    private String fabric;
+    private String filling;
+
+    public CustomPillow(Builder builder) {
+
+    }
+
+    public String CustomPillow(Builder builder){
+        this.name=builder.name;
+        this.color=builder.color;
+        this.weight=builder.weight;
+        this.fabric=builder.fabric;
+        this.filling=builder.filling;
+
+        return null;
+    }
+
+    static class Builder{
+        private final String name;
+        private String color;
+        private int weight;
+        private String fabric;
+        private String filling;
+
+
+        public Builder(String name) {
+            this.name = name;
+
+      }
+        public Builder color (String color){
+            this.color=color;
+            return this;
+        }
+        public Builder weight(int weight){
+            this.weight=weight;
+            return this;
+        }
+
+        public Builder fabric(String fabric){
+            this.fabric=fabric;
+            return  this;
+        }
+        public Builder filling(String filling){
+            this.filling=filling;
+            return this;
+        }
+        public CustomPillow build() {
+            return  new CustomPillow(this);
+        }
+    }
 }
+
--- clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code1/Main.java
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code1/Main.java
@@ -1,2 +1,28 @@
-package clean.code1;public class Main {
+package clean.code1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CustomPillow pillow01 = new CustomPillow.Builder("Rainbow")
+                .color("multicolor")
+                .weight(200)
+                .fabric("silk")
+                .filling("wadding")
+                .build();
+
+        CustomPillow pillow02 = new CustomPillow.Builder("SunShine")
+                .color("yellow") //am scos greutatea
+                .fabric("cloth")
+                .filling("wadding")
+                .build();
+
+        CustomPillow pillow03 = new CustomPillow.Builder("Aqua")
+                .color("blue")
+                .weight(212)
+                //am scos materialul
+                .filling("wadding")
+                .build();
+
+
+    }
 }
