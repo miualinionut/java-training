4b5181eeb831307fb7b107161a00b7147d8c7a98
Man Robert Daniel <robi_man41@yahoo.com>
2021-11-02 23:10:16 +0200

    prima incercare

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -1,8 +1,38 @@
-package code._4_student_effort;
-
-public class Main {
-
+public class Code_Challenge_1 {
     public static void main(String[] args) {
-        //TODO put your code changes in here
+        int i;
+        for(i = 1; i <= 100; i++)
+        {
+            if(i % 3 == 0)
+            {
+                if(i % 5 == 0)
+                    System.out.println("FizzBuzz");
+                else if(i % 7 == 0)
+                    System.out.println("FizzRizz");
+                else if(i % 11 == 0)
+                    System.out.println("FizzJazz");
+                else
+                    System.out.println("Fizz");
+            }
+            else if(i % 5 == 0)
+            {
+                if(i % 7 == 0)
+                    System.out.println("BuzzRizz");
+                else if(i % 11 == 0)
+                    System.out.println("BuzzJazz");
+                else
+                    System.out.println("Buzz");
+            }
+            else if(i % 7 ==0)
+                if(i % 11 == 0)
+                    System.out.println("RizJazz");
+                else
+                    System.out.println("Rizz");
+             else if(i % 11 == 0)
+                 System.out.println("Jazz");
+             else
+                 System.out.println(i);
+
+        }
     }
 }
