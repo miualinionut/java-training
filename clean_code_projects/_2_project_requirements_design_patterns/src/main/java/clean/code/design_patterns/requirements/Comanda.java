ac381d97eb2882a2fef16a26a29eec13f0f94be5
dragosmurea <dragos.murea24@gmail.com>
2021-12-12 20:22:41 +0200

    Add_BuilderPattern

---
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/Comanda.java
@@ -0,0 +1,74 @@
+package clean.code.design_patterns.requirements;
+
+public class Comanda {
+
+    private final int ID;
+    private final String numeCasier;
+    private final String meniu;
+    private final int pret ;
+    private final boolean pachet;
+    private final String data;
+
+    private Comanda(UserBuilder builder) {
+        this.ID = builder.ID;
+        this.numeCasier = builder.numeCasier;
+        this.meniu = builder.meniu;
+        this.pret = builder.pret;
+        this.pachet = builder.pachet;
+        this.data = builder.data;
+    }
+
+    //@Override
+    public void afisareBon() {
+
+        System.out.println("\n Bon fiscal: \n"
+                + "ID:"+this.ID+" \n"
+                + "Meniu:"+this.meniu + " \n "
+                + "Pret:"+this.pret+" \n "
+                + "Casier:"+this.numeCasier + " \n "
+                + "Data: " + this.data );
+        if (this.pachet)
+            System.out.println("La pachet");
+        else
+            System.out.println("Aici!");
+    }
+
+    public static class UserBuilder
+    {
+        private final int ID;
+        private  String meniu;
+        private  int pret ; // required
+        private  String numeCasier; // required
+        private  boolean pachet; // optional
+        private  final String data;
+
+
+        public UserBuilder(int ID,String data ) {
+            this.ID = ID;
+            this.data = data;
+        }
+
+        public UserBuilder meniu(String meniu) {
+            this.meniu = meniu;
+            return this;
+        }
+
+        public UserBuilder pret(int pret) {
+            this.pret = pret;
+            return this;
+        }
+        public UserBuilder numeCasier(String numeCasier) {
+            this.numeCasier = numeCasier;
+            return this;
+        }
+        public UserBuilder pachet(boolean pachet) {
+            this.pachet = pachet;
+            return this;
+        }
+
+        public Comanda build() {
+            Comanda user =  new Comanda(this);
+            return user;
+        }
+    }
+}
--- clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/Main.java
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/Main.java
@@ -11,5 +11,28 @@ public class Main {
 
         FastFood McDondaldsShoppingCityBuzau = new McDonalds();
         McDondaldsShoppingCityBuzau.buildfastfood();
+
+
+        Comanda comanda1 = new Comanda.UserBuilder(1,"13/12/2021" )
+
+                .numeCasier("Turcuman George")
+                .pachet(true)
+                .pret(40)
+                .meniu("2 * McFlurry, Chicken McGrill, Portie mica cartofi prajiti, sos dulce-acrisor, Cola mare")
+                .build();
+        comanda1.afisareBon();
+
+        System.out.println(comanda1);
+
+        Comanda comanda2 = new Comanda.UserBuilder(2, "14/12/2021")
+                .numeCasier("Iancu Paloma")
+                .pachet(false)
+                .meniu("Smart Meniu, Dublu hot buster, tarta ciocolota, Sprite mediu")
+                .pret(56)
+                .build();
+        comanda2.afisareBon();
+
+        System.out.println(comanda2);
+
     }
 }
