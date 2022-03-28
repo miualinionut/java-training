ffdcc1614b415ed2cb1d171e695685c96718d458
calinprecupetu <calinprecupetu@gmail.com>
2022-03-28 20:09:43 +0300

    Primul commit

--- _1_basics/src/main/java/code/_3_in_class/Main.java
+++ _1_basics/src/main/java/code/_3_in_class/Main.java
@@ -3,6 +3,6 @@ package code._3_in_class;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Salutari");
+        System.out.println("Salutari 2");
     }
 }
