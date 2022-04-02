4cb02f172ba83c31488e1fa4caf9f35d77a4d6cb
Dumitrescu Viorel-Nicolae <dterminator2020@gmail.com>
2022-04-02 12:28:01 +0300

    DUMITRESCU VIOREL-NICOLAE

---
+++ _1_basics/src/main/java/code/_4_student_effort/FizzBuzz.java
@@ -0,0 +1,46 @@
+package code._4_student_effort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static String Fizzbuzz(int i) {
+
+        String s=" ";
+
+        ArrayList<NumberWordMapping> mappings = new ArrayList<>();
+        NumberWordMapping map1 = new NumberWordMapping(3, "fizz");
+        NumberWordMapping map2 = new NumberWordMapping(5, "buzz");
+        NumberWordMapping map3 = new NumberWordMapping(7, "rizz");
+
+        mappings.add(map1);
+        mappings.add(map2);
+        mappings.add(map3);
+
+        boolean isDiv = false;
+
+        for(NumberWordMapping mapping : mappings){
+            if(i% mapping.number==0){
+                isDiv=true;
+                s+=mapping.word;
+            }
+        }
+        if(!isDiv){
+            s+=String.valueOf(i);
+        }
+
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        List<String> fizzbuzz = new ArrayList<>();
+        for(int i=1;i<=100;++i){
+            String t=Fizzbuzz(i);
+            fizzbuzz.add(t);
+        }
+        System.out.println(fizzbuzz);
+    }
+
+}
---
+++ _1_basics/src/main/java/code/_4_student_effort/NumberWordMapping.java
@@ -0,0 +1,11 @@
+package code._4_student_effort;
+
+public class NumberWordMapping {
+    int number;
+    String word;
+
+    NumberWordMapping(int number, String word){
+        this.number=number;
+        this.word=word;
+    }
+}
