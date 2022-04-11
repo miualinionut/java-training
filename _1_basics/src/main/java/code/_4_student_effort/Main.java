da95824f1a71b52362ee4df1ed455f8ad5725e69
Melania Iftinchi <melania.iftinchi@heavensolutions.com>
2022-04-11 13:49:25 +0300

    FizzBuzz

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,6 +3,27 @@ package code._4_student_effort;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO put your code changes in here
+        //FizzBuzz
+        
+        for (int i= 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else if(i % 7 == 0){
+                System.out.println("Rizz");
+            }
+            else if(i % 11 == 0){
+                System.out.println("Jazz");
+            }
+            else
+                System.out.println(i);
+        }
+
     }
 }
