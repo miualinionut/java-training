e5080b430f0298d1aa3b4817326309a781c8a175
Melania Iftinchi <melania.iftinchi@heavensolutions.com>
2022-04-11 14:07:53 +0300

    FizzBuzz

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -1,29 +1,14 @@
 package code._4_student_effort;
 
-public class Main {
+import code._2_challenge._1_fizzbuzz.FizzBuzz;
 
+public class Main {
     public static void main(String[] args) {
-        //FizzBuzz
-        
-        for (int i= 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            }
-            else if(i % 3 == 0){
-                System.out.println("Fizz");
-            }
-            else if(i % 5 == 0){
-                System.out.println("Buzz");
-            }
-            else if(i % 7 == 0){
-                System.out.println("Rizz");
-            }
-            else if(i % 11 == 0){
-                System.out.println("Jazz");
-            }
-            else
-                System.out.println(i);
-        }
+        Challenge challenge = new Challenge();
+        challenge.FizzBuzz();
+        // FooBarQix
+        for(int i = 1; i <= 53; i++)
+            System.out.println(challenge.compute(i));
 
     }
 }
