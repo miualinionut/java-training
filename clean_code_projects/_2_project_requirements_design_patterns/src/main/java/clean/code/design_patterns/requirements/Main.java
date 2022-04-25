030d809184c9e17bae006df17f343ace09eaf89d
daiaflorentina05 <daiaflorentina0502gmail.com>
2022-04-25 18:35:13 +0300

    Design Patterns

--- clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/Main.java
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/Main.java
@@ -1,8 +1,25 @@
 package clean.code.design_patterns.requirements;
+import model.Singer;
+
+
 
 public class Main {
 
-    public static void main(String[] args) {
-        //TODO implement your design patterns in this package
+        public static void main(String[] args) {
+            SingerFactory singerFactory = SingerFactory.getInstance();
+            Singer singer1 = singerFactory.createSinger("GUITAR");
+            Singer singer2 = singerFactory.createSinger("PIANO");
+
+            singer1.setName("Joe");
+            singer2.setName("Alice");
+            singer1.sing();
+            singer2.sing();
+
+            SingerFactory singerFactoryDoi =  SingerFactory.getInstance();
+            Singer singer3 = singerFactoryDoi.createSinger("GUITAR");
+            singer3.sing();
+
+            System.out.println("TOTAL SINGERS IN APP: " + SingerFactory.getInstance().getNumarSingeriCreati());
+        }
     }
-}
+
