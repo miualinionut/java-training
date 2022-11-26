f973891da0b60096bcd1631899c927202a52bb51
Alexandru Ivan <alextivan@gmail.com>
2022-11-26 10:44:50 +0200

    Hello world!

--- _1_basics/src/main/java/code/_3_in_class/HelloWorldClass.java
+++ _1_basics/src/main/java/code/_3_in_class/HelloWorldClass.java
@@ -2,6 +2,6 @@ package code._3_in_class;
 
 public class HelloWorldClass {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!(Alex Ivan)");
     }
 }
