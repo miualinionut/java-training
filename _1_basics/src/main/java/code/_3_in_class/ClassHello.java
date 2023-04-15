233bd67a1a8f36ef8647616d0e977d25e660010b
Cibu-Clara <clara_cibu@yahoo.com>
2023-04-15 11:02:24 +0300

    modified first Java class

--- _1_basics/src/main/java/code/_3_in_class/ClassHello.java
+++ _1_basics/src/main/java/code/_3_in_class/ClassHello.java
@@ -2,6 +2,6 @@ package code._3_in_class;
 
 public class ClassHello {
     public static void main(String[] args) {
-        System.out.println("Buna ziua");
+        System.out.println("Hello");
     }
 }
