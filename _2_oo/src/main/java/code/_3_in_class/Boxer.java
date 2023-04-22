1d352b27c541705a778c59c88f21e9b1052dc08d
ValentinDolcescu7 <valentindolcescu.vd@gmail.com>
2023-04-22 12:35:00 +0300

    classBoxerwithInterface

---
+++ _2_oo/src/main/java/code/_3_in_class/Boxer.java
@@ -0,0 +1,39 @@
+package code._3_in_class;
+
+public class Boxer implements IBoxer {
+    /*
+    State: name, health, damagePerAttack
+    Behaviour: attack, defend
+     */
+
+    String nume;
+    int health=100;
+    int damagePerAttack=10;
+
+    public Boxer(String nume, int health, int damagePerAttack)
+    {
+        this.nume=nume;
+        this.health=health;
+        this.damagePerAttack=damagePerAttack;
+    }
+    public Boxer(String nume)
+    {
+        this.nume=nume;
+    }
+
+    public void attack(Boxer opponent) {
+
+        opponent.health= opponent.health-this.damagePerAttack;
+        System.out.println(this.nume + " il ataca pe " + opponent.nume + " - new heath is = " + opponent.health);
+    }
+    public void defend() {
+
+    }
+    public String getNume() {
+        return this.nume;
+    }
+
+    public boolean isAlive() {
+        return this.health>0;
+    }
+}
--- _2_oo/src/main/java/code/_3_in_class/Main.java
+++ _2_oo/src/main/java/code/_3_in_class/Main.java
@@ -1,8 +1,40 @@
 package code._3_in_class;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        //TODO put your code changes in here
+
+        Boxer ion=new Boxer("Ion",100, 10);
+        Boxer vasile= new Boxer("Vasile");
+        Boxer bruceLee=new BruceLee("bruce lee");
+        //start meci
+        startBoxingMatch(ion, bruceLee);
+        //anuntam castigatorul
+        announceVictory(ion, bruceLee);
+
+    }
+
+    private static void announceVictory(IBoxer b01, IBoxer b02) {
+        if(b01.isAlive())
+        {
+            System.out.println(b01.getNume() + " a castigat meciul");
+
+        }else{
+            System.out.println(b02.getNume() + " a castigat meciu");
+        }
+    }
+
+    private static void startBoxingMatch(IBoxer ion, IBoxer vasile) {
+        Random random=new Random();
+        while(ion.isAlive() && vasile.isAlive()) {
+            int zeroOrOne = random.nextInt(4);
+            if (zeroOrOne == 1 || zeroOrOne==0) {
+                ion.attack((Boxer) vasile);
+            } else {
+                vasile.attack((Boxer) ion);
+            }
+        }
     }
 }
