23e38c626c721cd03fe9f99a59213d92937de96b
Diana <diana.adina81@gmail.com>
2023-04-22 14:08:04 +0300

    FizzBuzz Challenge

---
+++ _2_oo/src/main/java/code/_3_in_class/CorelatieNumarString.java
@@ -0,0 +1,11 @@
+package code._3_in_class;
+
+public class CorelatieNumarString {
+    int nr;
+    String word;
+    CorelatieNumarString(int nr, String word)
+    {   this.nr= nr;
+        this.word=word;
+
+    }
+}
