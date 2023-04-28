b8fdf96f9c90ac53f4596038597378977fcbdc24
raressichim <raressic@gmail.com>
2023-04-28 19:18:40 +0300

    Main modifications

--- _1_basics/src/main/java/code/_3_in_class/Main.java
+++ _1_basics/src/main/java/code/_3_in_class/Main.java
@@ -1,8 +1,43 @@
 package code._3_in_class;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        //TODO put your code changes in here
+        int[] ar = new int[]{1, 2, 3};
+        int[] v1 = copyArray(ar);
+        int[] v2 =copyArray(ar);
+        ar[0] = 10;
+        v1[0] = 100;
+        v2[0] = 1000;
+
+
+        displayVector("ar", ar);
+        displayVector("v1",v1);
+        displayVector("v2",v2);
+
+        String s="ABC";
+        System.out.println(s);
+        s="DEF";
+        System.out.println(s);
     }
+
+    private static int[] copyArray(int[]ar)
+    {
+        int[] copyArray=new int[ar.length];
+        for(int i=0;i<copyArray.length;i++)
+        {
+            copyArray[i]=ar[i];
+        }
+        return copyArray;
+    }
+    private static void displayVector(String name, int[] ar) {
+        System.out.print(name + " = ");
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(ar[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
