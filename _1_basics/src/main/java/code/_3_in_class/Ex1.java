838cd07340f8ed90963ee11ca90a279c4470b34c
weltheremanuel19 <weltheremanuel19@stud.ase.ro>
2023-05-20 00:20:12 +0300

    testare clasa

--- _1_basics/src/main/java/code/_3_in_class/Ex1.java
+++ _1_basics/src/main/java/code/_3_in_class/Ex1.java
@@ -1,7 +1,7 @@
 package code._3_in_class;
 
-public class Ex {
+public class Ex1 {
     public static void main(String[] args) {
-        System.out.println("salut");
+        System.out.println("salut lume1");
     }
 }
