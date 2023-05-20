5a16807a1f489037cd5829b04aa4d66772b7863a
911-Bercea-Andrei <mihai.bercea@stud.ubbcluj.ro>
2023-05-20 10:25:48 +0300

    Design Patterns - Bercea Andrei

--- _3_design_patterns/src/main/java/code/_4_student_effort/StockDataSubject.java
+++
@@ -1,7 +0,0 @@
-package code._4_student_effort;
-
-public interface StockDataSubject {
-    void registerObserver(StockDataObserver observer);
-    void removeObserver(StockDataObserver observer);
-    void notifyObservers(String stockName, double price);
-}
